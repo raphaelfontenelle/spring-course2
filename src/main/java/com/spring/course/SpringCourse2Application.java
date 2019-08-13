5d44f87599816551719841f857da5085f8af9391
Raphael Fontenelle <raphaelfontenelle@me.com>
2019-08-13 20:18:24 -0300

    Initial commit

---
+++ src/main/java/com/spring/course/SpringCourse2Application.java
@@ -0,0 +1,13 @@
+package com.spring.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCourse2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCourse2Application.class, args);
+	}
+
+}
