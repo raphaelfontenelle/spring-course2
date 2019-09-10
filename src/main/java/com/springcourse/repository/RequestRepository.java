6b16fe744a476f0acef0cdb4c62223b500544d01
Raphael Fontenelle <raphaelfontenelle@me.com>
2019-09-10 20:53:29 -0300

    Rename findOwnerId method name to findAllByOwnerIdName

--- src/main/java/com/springcourse/repository/RequestRepository.java
+++ src/main/java/com/springcourse/repository/RequestRepository.java
@@ -1,5 +1,20 @@
 package com.springcourse.repository;
 
-public interface RequestRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.spring.course.domain.Request;
+import com.spring.course.enums.RequestState;
+
+@Repository
+public interface RequestRepository extends JpaRepository<Request, Long>{
+
+	public List<Request> findAllByOwnerId(Long id);
+	
+	@Query("UPDATE Request SET state = ?2 WHERE id = ?!")
+	public Request updateStatus(Long id, RequestState state);
+	
 }
